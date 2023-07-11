a42319b8176b0cb99ef1369d8a5c7e242fc8cbf1
Jihyun <wlgus0260@naver.com>
2023-07-11 15:57:38 +0900

    feat: 피쳐된 포스트 보여주기 코드 구조

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import FeaturedPosts from './componenets/FeaturedPosts';
 import Hero from './componenets/Hero';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      {/* @ts-expect-error Async Server Component */}
+      <FeaturedPosts />
     </>
   );
 }
