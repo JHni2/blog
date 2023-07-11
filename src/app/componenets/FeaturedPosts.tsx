ed40617e8a253efa82f426878d77d416eb6891f4
Jihyun <wlgus0260@naver.com>
2023-07-11 16:17:21 +0900

    feat: 피쳐된 포스트 보여주기 카드

--- src/app/componenets/FeaturedPosts.tsx
+++ src/app/componenets/FeaturedPosts.tsx
@@ -1,11 +1,11 @@
-import { getAllPosts } from '../service/posts';
+import { getFeaturedPosts } from '../service/posts';
 import PostGrid from './PostGrid';
 
 export default async function FeaturedPosts() {
-  const posts = await getAllPosts();
+  const posts = await getFeaturedPosts();
   return (
     <section>
-      <h2>Featured Posts</h2>
+      <h2 className="text-2xl font-bold">Featured Posts</h2>
       <PostGrid posts={posts} />
     </section>
   );
---
+++ src/app/componenets/PostCard.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Post } from '../service/posts';
+import { data } from 'autoprefixer';
+
+type Props = { post: Post };
+
+export default function PostCard({ post: { title, description, date, category, path } }: Props) {
+  return (
+    <Link href={`/posts/${path}`}>
+      <Image src={`/images/posts/${path}.png`} alt={title} width={300} height={200}></Image>
+      <div>
+        <time>{date.toString()}</time>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <span>{category}</span>
+      </div>
+    </Link>
+  );
+}
