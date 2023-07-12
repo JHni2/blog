6bc9b932a310f685b5966523a037c940a4743f85
Jihyun <wlgus0260@naver.com>
2023-07-12 10:20:36 +0900

    feat: 정보, 연락 페이지 페이지

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,24 @@
+import Hero from '../componenets/Hero';
+
 export default function AboutPage() {
-  return <p>about</p>;
+  const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
+
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          공부하는 개발자 <br />
+          현재 NextJS 공부중!
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          React <br />
+          Git <br />
+          VS Code
+        </p>
+      </section>
+    </>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,28 @@
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import ContactForm from '../componenets/ContactForm';
+
 export default function ContactPage() {
-  return <p>contact</p>;
+  const LINKS = [
+    {
+      icon: <AiFillGithub />,
+      url: 'https://github.com/JHni2',
+    },
+    { icon: <AiFillLinkedin />, url: 'https://meteor-rainstorm-fce.notion.site/58f26f8d459b4cefbfd5818fd11fba68?pvs=4' },
+  ];
+
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>wlgus0260@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a key={index} href={link.url} target="_blank" rel="noreferrer" className="text-5xl hover:text-yellow-400">
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
